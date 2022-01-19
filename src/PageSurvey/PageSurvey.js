16e6f794516dd029e9d3bfe38e529d84f3fe6a93
dookda <sakda.homhuan@gmail.com>
2022-01-19 17:02:03 +0700

    update

---
+++ src/PageSurvey/PageSurvey.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./../assets/css/soft-ui-dashboard.min.css";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+
+const PageSurvey = () => {
+    return (
+        <div className='container'>
+            <Header name="สำรวจข้อมูล" />
+
+            <Footer />
+        </div>
+
+    )
+}
+
+export default PageSurvey;
